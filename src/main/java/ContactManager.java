25baf37d90ca39fe750fadd7255d8c3c6445d03f
Divin Joseph <101226867+Divin969@users.noreply.github.com>
2023-09-15 15:34:39 +0100

    .\Third Commit

--- src/main/java/ContactManager.java
+++ src/main/java/ContactManager.java
@@ -1,2 +1,31 @@
+import Contact.Contact;
+
+import java.util.HashMap;
+
 public class ContactManager {
+
+    HashMap<String, Contact> contact = new HashMap<String, Contact>();
+    public String getContactManager(getOperation) {
+        switch(getOperation) {
+            case "add" -> addContact();
+            case "remove" -> removeContact();
+            case "view" -> viewContact();
+            case "search" -> searchContact();
+        }
+
+        public void addContact() {
+            contact.put()
+
+        }
+        public void removeContact() {
+
+        }
+        public void viewContact() {
+
+        }
+        public void searchContact() {
+
+        }
+
+    }
 }
